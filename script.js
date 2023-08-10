0fd6083a446d5bba785eb4fb8252d24059e88049
Sami Hakani <sami.hakani@gmail.com>
2023-08-10 16:31:33 -0700

    Methods for grid generation and deletion

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 const gridContainer = document.querySelector('.grid-container');
 
-// methods
+// grid resize
+function gridGenerator(nSize) {
+    let grid = [];
 
-// grid creation
-let grid = []
-for (let i = 0; i < 16; i++) {
-    grid[i] = [];
-}
-
-for (let i = 0; i < 16; i++) {
-    let row = document.createElement('div'); // row container
-    row.classList.add('row')
-    for (let j = 0; j < 16; j++) {
-        grid[i][j] = document.createElement('div');
-        grid[i][j].classList.add('cell');
-        grid[i][j].addEventListener('mouseover', () => grid[i][j].style.backgroundColor = 'pink');
-
-        row.appendChild(grid[i][j]);
+    for (let i = 0; i < nSize; i++) {
+        grid[i] = [];
     }
-    gridContainer.appendChild(row);
+
+    for (let i = 0; i < nSize; i++) {
+        let row = document.createElement('div'); // row container
+        row.classList.add('row')
+        for (let j = 0; j < nSize; j++) {
+            grid[i][j] = document.createElement('div');
+            grid[i][j].classList.add('cell');
+            grid[i][j].addEventListener('mouseover', () => grid[i][j].style.backgroundColor = 'pink');
+            row.appendChild(grid[i][j]);
+        }
+        gridContainer.appendChild(row);
+    }
+
+    const gridSizeInPixels = 540;
+    const cellSizeInPixels = Math.round(gridSizeInPixels / nSize);
+    let cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+    cell.style.height = `${cellSizeInPixels}px`;
+    cell.style.width = `${cellSizeInPixels}px`;
+    });
 }
+
+function deleteGrid() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(firstChild);
+    }
+}
+
